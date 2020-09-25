f5299931f4632e0f4c8bc0d1d7cec39aea70ec57
arv-n <arvindnayak994@gmail.com>
2020-09-25 14:40:57 +0200

    Adding few more plots

--- beam_new.jl
+++ beam_new.jl
@@ -15,35 +15,71 @@ end
 
 # ╔═╡ e125c7d6-ef97-11ea-39fd-b176103f48ba
 begin 
-	using ForwardDiff
-	using Plots
 	using PlutoUI
 	using SparseArrays
+	using Arpack
+	using ForwardDiff
+	using Plots	
 	pyplot()
 end
 
 # ╔═╡ 219fcfc0-ef96-11ea-3baa-85eb1e81cf63
 md"# Euler Bernoulli Beams"
 
+# ╔═╡ 6646b4f0-fdbe-11ea-38a6-9ff1504f8f5d
+html"""
+<style>
+
+body.hide_all_inputs pluto-cell {
+	min-height: 0.5px;
+	margin-top: 6px;
+}
+</style>
+<script>
+document.body.classList.toggle("hide_all_inputs")
+</script>
+"""
+
 # ╔═╡ 7171003c-f65f-11ea-0ed1-eb09bbaa9a32
 begin
 	
 	md"""**Let us define the properties and loading characterstics of our beam**
 	
-	Analysis: $(@bind analysis Select(["static" => "Static" ,"dynamic" => "Dynamic"]))
-	
-	Type:  $(@bind t Select(["c" => "Cantilever" ,"ss" => "Simply Supported"]))
+	Analysis: $(@bind analysis Select(["static" => "Static" ,"dynamic" => "Dynamic"]))  Type:  $(@bind t Select(["c" => "Cantilever" ,"ss" => "Simply Supported"]))
 	
 	Length of the beam (L): 1 $(@bind L Slider(1:1:10;default=1)) 10
 
-	Number of nodes (N): 2 $(@bind N Slider(2:1:30;default=6)) 30
+	Number of nodes (N): 2 $(@bind N Slider(2:1:100;default=21)) 100
 	
-	Bending modulus (EI) = $(@bind ei NumberField(1:0.01:5;default=1))
+	Bending modulus (EI) = $(@bind ei NumberField(1:0.01:10;default=1))
 	
-	μ Mass/length = $(@bind μ NumberField(1:0.01:5;default=1))
+	μ Mass/length = $(@bind μ NumberField(1:0.01:5;default=1))"""
 	
-	Uniformly distributed Load (q) = $(@bind ul NumberField(-10:0.1:10;default=0))"""
+end
+
+# ╔═╡ 65d5625e-f58b-11ea-1cb7-fb37ec26e2c3
+begin
+	if t=="c"
+		md"""**Boundary Conditions for a cantilever beam**
 	
+			a = $(@bind a NumberField(0:2;default=0)) 
+	
+			b = $(@bind b NumberField(-1:1;default=0))  
+	
+			End Load (QL) = $(@bind Q NumberField(-10:0.01:10;default=1)) 
+	
+			End Moment (ML) = $(@bind ML NumberField(-1:0.01:1;default=0))"""
+	elseif t=="ss"
+		md"""**Boundary Conditions for a Simply Supported beam**
+			
+			a0 = $(@bind a NumberField(-10:0.01:10;default=0)) 
+	
+			aL = $(@bind b NumberField(-10:0.01:10;default=0))  
+	
+			Moment at x=0 (M0) = $(@bind Q NumberField(-10:0.01:10;default=-1))
+	
+			Moment at x=L (ML) = $(@bind ML NumberField(-10:0.01:10;default=1))"""
+		end
 end
 
 # ╔═╡ 4c1d7992-f668-11ea-01e2-6300503d267d
@@ -61,35 +97,11 @@ if analysis == "dynamic"
 end
 end
 
-# ╔═╡ 65d5625e-f58b-11ea-1cb7-fb37ec26e2c3
-begin
-	if t=="c"
-		md"""**Boundary Conditions for a cantilever beam**
-	
-			a = $(@bind a Slider(0:10)) 
-	
-			b = $(@bind b NumberField(-1:1;default=0))  
-	
-			End Load (Q) = $(@bind Q NumberField(-10:0.01:10;default=1)) 
-	
-			End Moment (M) = $(@bind ML NumberField(-1:0.01:1;default=0))"""
-	elseif t=="ss"
-		md"""**Boundary Conditions for a Simply Supported beam**
-			
-			a0 = $(@bind a html"<input type=range min=0 max=10 value=0>") 
-	
-			aL = $(@bind b html"<input type=number value=0>")  
-	
-			Moment at x=0 (M0) = $(@bind Q NumberField(-10:0.01:10;default=1))
-	
-			Moment at x=L (ML) = $(@bind ML NumberField(-10:0.01:10;default=1))"""
-		end
-end
+# ╔═╡ 58ce39ee-fe93-11ea-3244-f5d7f3935485
+md"""UDL (q) = $(@bind ul NumberField(-10:0.1:10;default=0))"""
 
 # ╔═╡ f2b09b22-f03a-11ea-2baf-ff3e1721752d
-md" And so the governing PDE for the above set of conditions will be <EDIT HERE>
-.
-We intend to solve this system with the Finite Element Method and analyze the results."
+md" And using these above parameters, we intend to solve the system with the `Finite Element Method`. "
 
 # ╔═╡ 00fbb85c-f508-11ea-21b0-1d20d4bdb05d
 md"#### Generate domain"
@@ -105,8 +117,16 @@ end
 # ╔═╡ 548dde12-f508-11ea-2ef8-fffbcd320ec8
 xh,ne,conn=gen_elements(L,N);
 
-# ╔═╡ 2b5fe7a2-f04a-11ea-3f33-07ef382f7722
-scatter(xh,10*ones(N),yaxis=nothing,ylims=(0,20),label="\$\\Omega\$",size=(700,200))
+# ╔═╡ c15ccb10-fdb7-11ea-2491-4557ac7265e5
+#scatter(xh,5*ones(N),ylims=(0,10),yaxis=nothing,size=(700,150),label="Ω")
+
+# ╔═╡ 2c02f7ba-f4ed-11ea-19c9-9bb69c5de35b
+function local2global(ne)
+	return [[2*e-1,2*e,2*e+1,2*e+2] for e=1:ne]
+end
+
+# ╔═╡ 998838b6-f508-11ea-0949-8b40b202bab8
+dofs=local2global(ne);
 
 # ╔═╡ 2b84ae9c-f66d-11ea-38e4-c1e6e9e7b7b8
 #Assumption of constant q,EI,mu throughout domain. Change for different distribution
@@ -182,19 +202,14 @@ function basis1D(xh,N)
 	return phi,phid,phidd
 end
 
-# ╔═╡ 2c02f7ba-f4ed-11ea-19c9-9bb69c5de35b
-function local2global(ne)
-	return [[2*e-1,2*e,2*e+1,2*e+2] for e=1:ne]
-end
-
 # ╔═╡ 3f7a5a08-f03e-11ea-1fcf-83e5b5e57d28
 phi,phid,phidd= basis1D(xh,N);
 
-# ╔═╡ 998838b6-f508-11ea-0949-8b40b202bab8
-dofs=local2global(ne);
-
 # ╔═╡ 098b7a0a-f2c1-11ea-31d6-91b4b485ac41
-md"###### Let us check whether our basis functions can approximate a function"
+md"###### Do you want to check whether our basis functions can approximate a function?"
+
+# ╔═╡ d8fa3576-fea1-11ea-127e-01b24b313b6b
+md"Check $(@bind approximate CheckBox()) "
 
 # ╔═╡ dbb675c8-f2b4-11ea-3ffa-81e83739270d
 function check(xh,f)
@@ -210,9 +225,11 @@ function check(xh,f)
 	scatter!(xh,p,label="piecewise cubic")
 end
 
+# ╔═╡ 3f0f1d7a-fea2-11ea-1d7d-bffce1d81fec
+f(x)=cos(x)*sin(x) ##Give a function
+
 # ╔═╡ c78ce07a-f2bc-11ea-28bd-a1df0c486ed1
-begin
-	f(x)=cos(x)*sin(x)
+if approximate
 	check(xh,f::Function);
 end
 
@@ -366,13 +383,10 @@ function dyn_assemble(mu,EI,Q,ML,qv,xh,ne,conn,phi,phidd,dofs)
 		
 	#Extended matrix system with BCs
 	S_e = [S C; C' zeros(2,2)];
-	M_e = [S 0*C; 0*C' zeros(2,2)];
+	M_e = [M 0*C; 0*C' zeros(2,2)];
 	
 	return S_e,M_e,q_e;
 end	
-
-# ╔═╡ b59aacb2-f50d-11ea-34dd-67c479511398
-md"### Solving the system"
 
 # ╔═╡ b9cb119e-f5e8-11ea-302d-6db9fec26b2b
 function newmark_step(Mtau,xh,w,tau,beta,gamma,M_e,S_e,phi)
@@ -388,29 +402,52 @@ function newmark_step(Mtau,xh,w,tau,beta,gamma,M_e,S_e,phi)
 		udotdot = (M_e + (beta*tau^2)*S_e)\b;
 		w = ustar + beta*tau^2*udotdot;
 		udot = ustardot + gamma*tau*udotdot;
+		
 		w_interp[:,i] = sum((w[2*j-1]*phi[2*j-1].(xh) + w[2*j]*phi[2*j].(xh)) for j=1:length(xh));
 	end
 	return w_interp
 end
+
+# ╔═╡ b59aacb2-f50d-11ea-34dd-67c479511398
+md"### Solving the system"
 
 # ╔═╡ 28da6d86-f4f7-11ea-010d-f7838116054e
 begin
 	if analysis=="static"
 		S_e,q_e = assemble(t,EI,Q,ML,qv,a,b,xh,ne,conn,phi,phidd,dofs);
 		w = S_e\q_e;
-		w_interp = sum((w[2*j-1]*phi[2*j-1].(xh) + w[2*j]*phi[2*j].(xh)) for j=1:length(xh));
+		w_interp = sum((w[2*j-1]*phi[2*j-1].(xh) + w[2*j]*phi[2*j].(xh)) for j=1:N);
 		
 	elseif analysis=="dynamic"
 		S_e,M_e,q_e = dyn_assemble(mu,EI,Q,ML,qv,xh,ne,conn,phi,phidd,dofs);
 		w = S_e\q_e;
-		w_interp = newmark_step(Mtau,xh,w,tau,beta,gamma,M_e,S_e,phi);
-		
+		w_interp = newmark_step(Mtau,xh,w,tau,beta,gamma,M_e,S_e,phi);	
 	end
 	nothing
 end
 
 # ╔═╡ e2536ecc-f674-11ea-386d-274c40e5a846
 md"### Results and Post Processing"
+
+# ╔═╡ 1b080bca-fed5-11ea-169f-5930c4037901
+md"###### Let us analyze the results!"
+
+# ╔═╡ b2b57896-f696-11ea-07b0-2f01a202fbd0
+function plotFEM(xh,wh,plt_title="Static Case",plt_label="FEM",ylim=[-L/2,L/2])
+	#assumption of linear interpolation in between nodes while plotting. 
+	plot(xh,wh,
+		xlims=(0,L+0.05),
+		ylims=(ylim[1],ylim[2]),
+		label=plt_label,
+		xlabel="Domain (Ω)",
+		ylabel="Deflection (w)",
+		marker=(:circle,:orange),title=plt_title)
+end	
+
+# ╔═╡ a32eccb4-f6ab-11ea-2b2c-1de81ae66813
+if analysis=="static"
+	md"#### The Static Case"
+end
 
 # ╔═╡ 1b42f33a-f510-11ea-128a-c5dadf723476
 function check_analytical(t,a,b,Q,ML,q,L,ei,xh)
@@ -426,6 +463,9 @@ function check_analytical(t,a,b,Q,ML,q,L,ei,xh)
 			elseif iszero(q) && iszero(Q)
 				w_exact = (ML*xh.^2)/(2*ei)
 				text="with end moment"
+			elseif iszero(q)
+				w_exact = (3*ML .+ 3*L*Q .- Q*xh).*xh.*xh/6*ei
+				text="with end moment and load"
 			else 
 				text="Analytical solution not coded in!"
 			end
@@ -443,40 +483,28 @@ function check_analytical(t,a,b,Q,ML,q,L,ei,xh)
 	else
 		text="Analytical solution not coded in!"
 	end
-	
+	w_exact=w_exact +  a*ones(length(xh))
 return w_exact,text;
 end
 
-# ╔═╡ b2b57896-f696-11ea-07b0-2f01a202fbd0
-function plotFEM(xh,wh,ylim=[-L/2,L/2])
-	#assumption of linear interpolation in between nodes while plotting. 
-	plot(xh,wh,
-		xlims=(0,L+0.05),
-		ylims=(ylim[1],ylim[2]),
-		label="FEM",
-		xlabel="Domain (Ω)",
-		ylabel="Deflection (w)",
-		marker=(:circle,:orange),title="Static Case")
-end	
-
-# ╔═╡ a32eccb4-f6ab-11ea-2b2c-1de81ae66813
-md"#### Static"
+# ╔═╡ ae561da6-fe92-11ea-2d88-03ef3f13dff8
+if analysis=="static"
+md"make sure you uncheck the options before seeing new plots, otherwise the plot! will overlap on the same image. Julia `Plots()` package artifact :("
+end
 
 # ╔═╡ f64ad968-f68f-11ea-2a36-7b7bdac2b87d
 if analysis=="static"
 		md"""**Check Analytical solution** $(@bind check_sol CheckBox())
-			 **Plot Shear Force distribution** $(@bind shear_stress CheckBox())
-			 **Plot Bending Moment distribution** $(@bind bending_moment CheckBox())
 	"""
 end
 
 # ╔═╡ 68166060-f5b1-11ea-0e76-41f2220403c6
 begin
 	if analysis=="static"
-	y_lim=[minimum(w_interp) maximum(w_interp)] 
+	y_lim=[minimum(w_interp)-a maximum(w_interp)] 
 	y_lim = y_lim+0.5*y_lim
 		if check_sol==false
-			plotFEM(xh,w_interp,y_lim)
+			plotFEM(xh,w_interp,"Static Case","FEM Solution",y_lim)
 		else 
 			w_ex, ctext = check_analytical(t,a,b,Q,ML,ul,L,ei,xh)
 			plot!(xh,w_ex,
@@ -486,60 +514,99 @@ begin
 				)
 		end
 		else
-			"Select the static case to see these plots"
+			md"Select the static case to see the static plots!"
 	end
 end
 
 # ╔═╡ 96f1baee-f6ab-11ea-2dd3-57f8e2e541d8
-md"#### Dynamic"
-
-# ╔═╡ a6cb3bd2-f6a1-11ea-09e0-198c23e9b2a8
 if analysis=="dynamic"
-	anim = @animate for T ∈ 1:Mtau
-		plot(xh,w_interp[:,T],
-			xlims=(0,L+0.05),
-			ylims=(-1,1),
-			label="FEM solution",
-			xlabel="Domain (Ω)",
-			ylabel="Deflection (w)",
-			marker=(:circle,:orange),
-			annotations=(L/3,L/1.15,"T=$(T)"))
-		plot!(title="Dynamic case")
-	end
-	gif(anim)
-else 
-	"select the dynamic case to see this clip"
+md"#### The Dynamic Case - Free and Undamped Vibrations"
 end
 
 # ╔═╡ 16792298-f6a5-11ea-07a8-39d00521b9db
 if analysis=="dynamic"
-	md"#### Plotting first five modes"
+	md"#### Plotting the first 5 modes"
 end
 
-# ╔═╡ f0bebeb0-f6a8-11ea-2eaf-a72972cd414e
+# ╔═╡ 34eec6f6-fd9e-11ea-1563-e1a9c54012ad
+if analysis=="dynamic"
+	if t=="c"
+		idx=3:2*N
+	elseif t=="ss"
+		part=[i for i in 3:(2*N-1)]
+		idx=pushfirst!(part,1)
+	end
+	
+	λ, ϕ = eigs(S_e[idx,idx],M_e[idx,idx]; nev=N,which=:SM);
+	eigenpairs = [(λ[i], ϕ[:,i]) for i in 1:length(λ)];
+	sort!(eigenpairs, by=pair -> abs(pair[1]))
+	nothing
+end
 
+# ╔═╡ d2faeeb8-fdff-11ea-3fab-8d961cba8f25
+begin
+	if analysis=="dynamic"
+		no_of_eigs=5
+		w_interp_eig=zeros(N,no_of_eigs)
+		for eig_i in 1:no_of_eigs
+			w_mode = zeros(2*N)
+			val, vect = eigenpairs[eig_i]
+			vect = vect*sign(vect[5])
+			w_mode[idx] = vect[:]
+			w_interp_eig[:,eig_i] = sum((w_mode[2*j-1]*phi[2*j-1].(xh) + w_mode[2*j]*phi[2*j].(xh)) for j=1:N)
+		end
+		if t=="c"
+			plotFEM(xh,w_interp_eig,"Mode Shapes-Cantilever",["Mode 1" "Mode 2" "Mode 3" "Mode 4" "Mode 5"])
+		elseif t=="ss"
+			plotFEM(xh,w_interp_eig,"Mode Shapes-Simply Supported",["Mode 1" "Mode 2" "Mode 3" "Mode 4" "Mode 5"])
+		end
+	end
+end
+
+# ╔═╡ 0af93070-fda3-11ea-06a3-6b34638a8696
+if analysis=="dynamic"
+	md"**Create Movie** $(@bind click CheckBox()) "
+end
+
+# ╔═╡ 5c0cb724-fec9-11ea-17de-817c5a58be19
+if click
+	if analysis=="dynamic"
+		anim = @animate for T ∈ 1:Mtau
+			plotFEM(xh,w_interp[:,T],
+				"Free vibrations","FEM solution",[-1,1])
+			plot!(annotations=(L/3,L/1.15,"T=$(T)"))
+		end
+		gif(anim)
+	else 
+		"select the dynamic case to see this clip"
+	end
+end
 
 # ╔═╡ Cell order:
 # ╟─219fcfc0-ef96-11ea-3baa-85eb1e81cf63
-# ╠═e125c7d6-ef97-11ea-39fd-b176103f48ba
+# ╟─e125c7d6-ef97-11ea-39fd-b176103f48ba
+# ╟─6646b4f0-fdbe-11ea-38a6-9ff1504f8f5d
 # ╟─7171003c-f65f-11ea-0ed1-eb09bbaa9a32
-# ╟─4c1d7992-f668-11ea-01e2-6300503d267d
 # ╟─65d5625e-f58b-11ea-1cb7-fb37ec26e2c3
+# ╟─4c1d7992-f668-11ea-01e2-6300503d267d
+# ╟─58ce39ee-fe93-11ea-3244-f5d7f3935485
 # ╟─f2b09b22-f03a-11ea-2baf-ff3e1721752d
 # ╟─00fbb85c-f508-11ea-21b0-1d20d4bdb05d
 # ╟─16101166-f4de-11ea-25f8-bdfa0e83d2a4
 # ╠═548dde12-f508-11ea-2ef8-fffbcd320ec8
-# ╠═2b5fe7a2-f04a-11ea-3f33-07ef382f7722
+# ╠═c15ccb10-fdb7-11ea-2491-4557ac7265e5
+# ╟─2c02f7ba-f4ed-11ea-19c9-9bb69c5de35b
+# ╠═998838b6-f508-11ea-0949-8b40b202bab8
 # ╟─2b84ae9c-f66d-11ea-38e4-c1e6e9e7b7b8
 # ╠═8cbd99bc-f66d-11ea-3c74-2b9b9999741d
 # ╟─02a16210-f503-11ea-2552-7b72536ee06d
 # ╟─466ce928-f03b-11ea-1b7e-a9e26cbccdcc
-# ╟─2c02f7ba-f4ed-11ea-19c9-9bb69c5de35b
 # ╠═3f7a5a08-f03e-11ea-1fcf-83e5b5e57d28
-# ╠═998838b6-f508-11ea-0949-8b40b202bab8
 # ╟─098b7a0a-f2c1-11ea-31d6-91b4b485ac41
+# ╟─d8fa3576-fea1-11ea-127e-01b24b313b6b
 # ╟─dbb675c8-f2b4-11ea-3ffa-81e83739270d
-# ╠═c78ce07a-f2bc-11ea-28bd-a1df0c486ed1
+# ╠═3f0f1d7a-fea2-11ea-1d7d-bffce1d81fec
+# ╟─c78ce07a-f2bc-11ea-28bd-a1df0c486ed1
 # ╟─04fb4452-f2c1-11ea-393c-4b9b88d2aaf8
 # ╟─9aea9122-f5d0-11ea-1bc7-d5b5f6368033
 # ╟─6c4aa6b8-f39b-11ea-0d7e-6bf87aaa95be
@@ -549,16 +616,20 @@ end
 # ╟─680184dc-f422-11ea-3aa4-e7d07918b79b
 # ╟─84c0f1b8-f5d0-11ea-14e6-c987ddf8da67
 # ╟─986616bc-f5dd-11ea-3975-bb8d25062cce
-# ╟─b59aacb2-f50d-11ea-34dd-67c479511398
 # ╟─b9cb119e-f5e8-11ea-302d-6db9fec26b2b
-# ╟─28da6d86-f4f7-11ea-010d-f7838116054e
+# ╟─b59aacb2-f50d-11ea-34dd-67c479511398
+# ╠═28da6d86-f4f7-11ea-010d-f7838116054e
 # ╟─e2536ecc-f674-11ea-386d-274c40e5a846
-# ╟─1b42f33a-f510-11ea-128a-c5dadf723476
-# ╟─b2b57896-f696-11ea-07b0-2f01a202fbd0
+# ╟─1b080bca-fed5-11ea-169f-5930c4037901
+# ╠═b2b57896-f696-11ea-07b0-2f01a202fbd0
 # ╟─a32eccb4-f6ab-11ea-2b2c-1de81ae66813
+# ╟─1b42f33a-f510-11ea-128a-c5dadf723476
+# ╟─ae561da6-fe92-11ea-2d88-03ef3f13dff8
 # ╟─f64ad968-f68f-11ea-2a36-7b7bdac2b87d
 # ╟─68166060-f5b1-11ea-0e76-41f2220403c6
 # ╟─96f1baee-f6ab-11ea-2dd3-57f8e2e541d8
-# ╟─a6cb3bd2-f6a1-11ea-09e0-198c23e9b2a8
 # ╟─16792298-f6a5-11ea-07a8-39d00521b9db
-# ╠═f0bebeb0-f6a8-11ea-2eaf-a72972cd414e
+# ╟─34eec6f6-fd9e-11ea-1563-e1a9c54012ad
+# ╟─d2faeeb8-fdff-11ea-3fab-8d961cba8f25
+# ╟─0af93070-fda3-11ea-06a3-6b34638a8696
+# ╟─5c0cb724-fec9-11ea-17de-817c5a58be19
