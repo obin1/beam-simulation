c0ebda7150757e143da597a01a8921ca6857d0c7
arv-n <arvindnayak994@gmail.com>
2021-01-05 18:02:57 +0100

    updating README

--- start_notebooks.jl
+++ start_notebooks.jl
@@ -1,6 +1,13 @@
 import Pkg
 Pkg.activate(@__DIR__)
 
+import PlutoUI
+import SparseArrays
+import LinearAlgebra
+import Arpack
+import ForwardDiff
+import PyPlot
+
 import Pluto
 println("From the Pluto main menu, open the file:")
 println(joinpath(@__DIR__, "Beam.jl"))
